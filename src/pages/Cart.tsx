2f689e7d2f10d9261eb60a0a4004fa85239e0652
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-06-30 15:48:17 -0300

    starting Cart logic

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,21 @@
-
+import { useCart } from "../hooks/useCart"
 
 const Cart = () => {
+    const {removeFromCart, clearCart, cartTotal, cartItemCount} = useCart();
 
     return (
-        <div className="cart-container">Cart</div>
+        <div className="cart-container">
+            <div className="item-list"></div>
+            <div className="checkout-panel">
+                <div 
+                className="total-price"
+                >Total: <span className="">{cartTotal}</span>
+                </div>
+                <div 
+                className="total-quantity"
+                >Quantity: <span className="">{cartItemCount}</span></div>
+            </div>
+        </div>
     )
 }
 
