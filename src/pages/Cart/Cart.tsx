4f09c71a52ac1d8a11c1d050ea40c0d1a0676cd3
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-07-09 11:42:08 -0300

    adding React portal to backdrop

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -18,13 +18,38 @@ const Cart = () => {
                 }
             </div>
             <div className={styles.checkoutPanel}>
-                <div 
-                className={styles.totalPrice}
-                >Total: <span className="">{cartTotal.toFixed(2)}</span>
-                </div>
+                <h3 className={styles.checkoutH3}>Temdetudo</h3>
+                <h4 className={styles.checkoutH4}>Cart content:</h4>
                 <div 
                 className={styles.totalQuantity}
                 >Quantity: <span className="">{cartItemCount.toFixed(0)}</span></div>
+                <div>
+                    <div 
+                    className={styles.productPriceContainer}
+                    >Products: US$
+                        <span 
+                        className={styles.productPriceSpan}
+                        >{cartTotal.toFixed(2)}
+                        </span>
+                    </div>  
+                    <div
+                    className={styles.taxesContainer}
+                    >Taxes: 
+                        <span
+                        className={styles.taxesSpan}
+                        >{(cartTotal*0.06).toFixed(2)}
+                        </span>
+                    </div>
+                    <div
+                    className={styles.totalContainer}
+                    >Total: 
+                        <span 
+                        className={styles.totalSpan}
+                        >{(cartTotal*0.06 + cartTotal).toFixed(2)}
+                        </span>
+                    </div>
+                </div>
+                <button>Proceed to checkout</button>
             </div>
         </div>
     )
